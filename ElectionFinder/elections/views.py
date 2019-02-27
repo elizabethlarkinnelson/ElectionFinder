210d0611be0b67e1e1acf88fe8e7344055c60f8d
elizabethlarkinnelson <elizabeth.larkin.nelson@gmail.com>
2019-02-27 15:29:18 -0800

    Response rendes

--- ElectionFinder/elections/views.py
+++ ElectionFinder/elections/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .utils import create_dw_api
 import requests
+import json
 
 from .forms import ElectionForm
 
@@ -14,14 +15,30 @@ def election_form(request):
             state = address['state'].strip().lower()
             place = address['city'].strip().replace(" ", "_").lower()
 
-            r = requests.get(create_dw_api(state, place))
-            s = r.status_code
+            r = requests.get(create_dw_api(state, place),headers={'Accept': 'application/json' })
+            if r.status_code == 200:
+                r_text = r.text
+                r_jsonify = json.loads(r_text)
 
-            context = {
-                'state': state,
-                'place': place,
-                's': s,
-            }
+                if r_jsonify:
+                    r_dict = r_jsonify[0]
+
+                    context = {
+                        'state': state,
+                        'place': place,
+                        'message': 'Whoo hooo! There is an election in ',
+                        'date': 'Here is the date:',
+                        'election_info': r_dict['date'][:10],
+                    }
+                else:
+                    context = {
+                    'sorry' : 'Sorry, no upcoming elections in your area',
+                }
+
+            else:
+                context = {
+                    'sorry' : 'Sorry, those are not valid places',
+                }
             return render(request, 'elections/result.html', context)
     else:
         form = ElectionForm()
